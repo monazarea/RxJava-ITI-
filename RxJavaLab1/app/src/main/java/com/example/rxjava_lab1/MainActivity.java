17ba624a6a59bfd700a065a959037fe944b82bfd
monazarea <94869444+monazarea@users.noreply.github.com>
2026-02-02 13:30:03 +0200

    add Lab1 (FromArray & FromIterable)

---
+++ RxJavaLab1/app/src/main/java/com/example/rxjava_lab1/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.rxjava_lab1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    Button btn_Array;
+    Button btn_Iterable;
+    TextAdapter adapter;
+    ArrayList<String> list = new ArrayList<>();
+    private Integer[] arrays = {0, 1, 2, 3, 4, 5};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycleView);
+        btn_Array = findViewById(R.id.button_array);
+        btn_Iterable = findViewById(R.id.button_iterable);
+        adapter = new TextAdapter(list);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+
+        btn_Array.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Observable<Integer> observable = io.reactivex.rxjava3.core.Observable.fromArray(arrays);
+                observable.subscribe(
+                        item -> {
+                            list.add("From Array: onNext: " + item);
+                            adapter.notifyDataSetChanged();
+                        },
+                        error -> {
+                            list.add("From Array: onError: " + error.getMessage());
+                            adapter.notifyDataSetChanged();
+                        },
+                        () -> {
+                            list.add("From Array: onComplete");
+                            adapter.notifyDataSetChanged();
+                        }
+                );
+
+            }
+        });
+        btn_Iterable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Observable<Integer> observable = io.reactivex.rxjava3.core.Observable.fromIterable(Arrays.asList(arrays));
+                observable.subscribe(
+                        item -> {
+                            list.add("From Iterable: onNext: " + item);
+                            adapter.notifyDataSetChanged();
+                        },
+                        error -> {
+                            list.add("From Iterable: onError: " + error.getMessage());
+                            adapter.notifyDataSetChanged();
+                        },
+                        () -> {
+                            list.add("From Iterable: onComplete");
+                            adapter.notifyDataSetChanged();
+                        }
+                );
+            }
+        });
+
+    }
+}
---
+++ RxJavaLab1/app/src/main/java/com/example/rxjava_lab1/TextAdapter.java
@@ -0,0 +1,47 @@
+package com.example.rxjava_lab1;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+public class TextAdapter extends RecyclerView.Adapter<TextAdapter.ViewHolder> {
+
+    private List<String> items;
+
+    public TextAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_text, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textView.setText(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.tvItem);
+        }
+    }
+}
+
